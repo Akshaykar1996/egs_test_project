aa5068608cdc27ed60f1d4e931dc6585def6606e
Akshaykar1996 <akshaykumarkar89@gmail.com>
2024-09-14 12:38:12 -0400

    project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navber from './componet/Navber/Navber';
 import Navbersec from './componet/Navber/Navbersec';
 import Herosection from './componet/Handpicked/Herosection';
 import { Groups } from './componet/Groups/Groups';
-import Addsection from './Addsection/Addsection';
 import { Wright } from './componet/Wright/Wright';
 import { Footer } from './componet/Footer/Footer';
+import Addsection from './componet/Addsection/Addsection';
 
 
 function App() {
